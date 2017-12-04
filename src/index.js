99a821a769049009f7b6dff60f44e6ebdf316818
Ronald <rchangmh@gmail.com>
2017-12-04 15:43:19 -0500

    attempting to add apollo

--- src/index.js
+++ src/index.js
@@ -2,10 +2,49 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import gql from 'graphql-tag'
+
+const local = withClientState({
+  Query: {
+    todos: () => []
+  },
+  Mutation: {
+    addTodo: (_, { message, title }, { cache }) => {
+      const current = cache.readQuery({ query, variables })
+      const data = {
+        todos: current.todos.concat([{ message, title, __typename: 'Todo' }])
+      }
+      cache.writeQuery({ query, variables, data })
+      return null
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: local,
+  cache: new InMemoryCache()
+})
+
+const query = gql`
+  query todos {
+    todos @client {
+      message
+      title
+    }
+  }
+`
+
+const initial = await client.query({ query })
 
 render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  , document.getElementById('root')
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
+  document.getElementById('root')
 )

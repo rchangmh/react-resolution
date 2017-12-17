e517931665388ad633df31aed3356856fb536fb2
Ronald <rchangmh@gmail.com>
2017-12-16 20:18:13 -0500

    add new goals

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Button, Header } from 'semantic-ui-react'
-import { Link } from 'react-router'
+import { Link } from 'react-router-dom'
 
 export default class Add extends Component {
 	state = {
@@ -11,7 +11,7 @@ export default class Add extends Component {
 		return (
 			<div>
 				<Header>Add goals!</Header>
-				<Button as={Link} to="/new" size="large" icon="add" />
+				<Button as={Link} to="/add/new" size="large" icon="add" />
 			</div>
 		)
 	}
--- src/containers/New.js
+++ src/containers/New.js
@@ -1,2 +1,62 @@
 import React, { Component } from 'react'
 import GetInput from '../components/GetInput'
+import { Link } from 'react-router-dom'
+import { Button, Form, Header } from 'semantic-ui-react'
+
+export default class New extends Component {
+	state = {
+		activity: '_____',
+		goal: '_____',
+		metric: '_____',
+		amountDedicated: '_____'
+	}
+
+	handleChange = value => {
+		this.setState({ metric: value })
+		console.log(this.state)
+	}
+
+	handleSubmit = () => {
+		console.log(this.state)
+	}
+
+	render() {
+		return (
+			<div>
+				<Header>{`I want to ${this.state.activity} ${this.state.goal} ${this
+					.state.metric} this year.`}</Header>
+				<p>
+					Example: I want to <u>exercise</u> <u>180</u> <u>days</u> this year.
+				</p>
+				<Form.Input
+					required
+					placeholder="activity"
+					onChange={event => this.setState({ activity: event.target.value })}
+				/>
+				<Form.Input
+					required
+					placeholder="goal"
+					onChange={event => this.setState({ goal: event.target.value })}
+				/>
+				<Form.Input
+					required
+					placeholder="metric"
+					onChange={event => this.setState({ metric: event.target.value })}
+				/>
+				<Header>{`I want to dedicate $${this.state
+					.amountDedicated} to this goal.`}</Header>
+				<Form.Input
+					required
+					placeholder="0.00"
+					icon="dollar"
+					iconPosition="left"
+					onChange={event =>
+						this.setState({ amountDedicated: event.target.value })}
+				/>
+				<Form.Button as={Link} onClick={this.handleSubmit()}>
+					Submit
+				</Form.Button>
+			</div>
+		)
+	}
+}
